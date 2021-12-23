07679ba20b5cac0b6273e48258c4a5a149e550a3
Алексей Андрюшин <aleksroebuck@gmail.com>
2021-12-23 22:33:11 +0200

    Updated admin.py file in orders app

--- orders/admin.py
+++ orders/admin.py
@@ -2,15 +2,14 @@ from django.contrib import admin
 from .models import Order, OrderItem
 
 
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ('product',)
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid']
-    list_filter = ['created', 'paid']
+    list_filter = ['created', 'paid', 'updated']
     list_editable = ['paid']
-
-
-@admin.register(OrderItem)
-class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ['product', 'price', 'quantity']
-    list_filter = ['quantity']
-    list_editable = ['price', 'quantity']
+    inlines = [OrderItemInline, ]
