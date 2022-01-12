865b6b2f972219b684947a932eba4fb719d52553
Алексей Андрюшин <aleksroebuck@gmail.com>
2022-01-12 13:23:13 +0200

    Implemented CSV and PDF export of orders. Added admin detail view

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.conf.urls import url
+from django.urls import path, re_path
 from . import views
 
 app_name = 'orders'
 
 urlpatterns = [
     path('create/', views.order_create, name='order_create'),
+    url(r'^admin/order/(?P<order_id>\d+)/$', views.admin_order_detail, name='admin_order_detail'),
 ]
--- shop_project/settings.py
+++ shop_project/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'cart',
     'orders',
     'shop_api',
-    'rest_framework'
+    'rest_framework',
+    'coupons',
 ]
 
 MIDDLEWARE = [
